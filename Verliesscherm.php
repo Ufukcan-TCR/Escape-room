82c5c12c7fb3e3908cb2580a5d9858b8ba7620dd
NeoNizar <9027954@student.zadkine.nl>
2026-06-02 13:37:30 +0200

    winscherm verliesscherm css apart nemen

--- Verliesscherm.php
+++ Verliesscherm.php
@@ -4,31 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Je hebt gefaald...</title>
-    <style>
-        body {
-            margin: 0;
-            font-family: 'Comic Sans MS', sans-serif;
-            background: linear-gradient(to bottom, #8b0000, #300000);
-            color: #333;
-            text-align: center;
-        }
-        .message-box {
-            background-color: rgba(100, 100, 100, 0.8);
-            display: inline-block;
-            padding: 20px 40px;
-            margin-top: 200px;
-            margin-bottom: 400px;
-            border-radius: 10px;
-            color: #000;
-        }
-        .message-box h1 {
-            font-family: 'Impact', sans-serif;
-        }
-        .bottom-icons {
-            font-size: 25px;
-            margin-top: 60px;
-        }
-    </style>
+    <link rel="stylesheet" href="css/verliesscherm.css">
+
 </head>
 <body>
     <div class="message-box">
